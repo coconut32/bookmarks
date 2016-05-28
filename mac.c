593599a2711f2a58fe89488aaf718cefe1e2074d
coconut32 <coconut_liu@126.com>
2016-05-28 16:31:54 +0800

    Create mac.c

---
+++ mac.c
@@ -0,0 +1,16 @@
+//android
+Using PhoneStateListener to listen to state change in Android http://www.devlper.com/2010/06/using-phonestatelistener-to-listen-to-state-change-in-android/
+
+//reverse
+就算Lua也hook给你看 http://bbs.pediy.com/showthread.php?t=163435
+reverse engineer blog http://www.kdsbest.com/?p=117
+UltraEdit 16.10.0.1028的反破解保护 http://www.pediy.com/kssd/pediy11/119807.html
+把qq2005做成后门之编程实现 http://www.360doc.com/content/06/0910/18/10063_203950.shtml
+修改windows自带的calc.exe http://blog.csdn.net/kendyhj9999/article/details/7166410
+
+//google
+Unofficial Google Advanced Search http://jwebnet.net/advancedgooglesearch.html
+DMCA (Copyright) Complaint to Google https://www.lumendatabase.org/notices/660366#
+
+//msic blog
+program,math and more http://dbfin.com/
