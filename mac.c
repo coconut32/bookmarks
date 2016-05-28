e30810b51a67b1649ed24466e5f199a1a28510f7
coconut32 <coconut_liu@126.com>
2016-05-28 18:36:05 +0800

    Update mac.c

--- mac.c
+++ mac.c
@@ -7,6 +7,7 @@ reverse engineer blog http://www.kdsbest.com/?p=117
 UltraEdit 16.10.0.1028的反破解保护 http://www.pediy.com/kssd/pediy11/119807.html
 把qq2005做成后门之编程实现 http://www.360doc.com/content/06/0910/18/10063_203950.shtml
 修改windows自带的calc.exe http://blog.csdn.net/kendyhj9999/article/details/7166410
+A modest proposal (Absolutely no babies involved) https://www.reddit.com/r/ReverseEngineering/comments/hg0fx/a_modest_proposal_absolutely_no_babies_involved/
 
 //google
 Unofficial Google Advanced Search http://jwebnet.net/advancedgooglesearch.html
@@ -28,6 +29,9 @@ Algebraic Combinatorics http://www.math.cornell.edu/~levine/18.312/
 The Theoretical Minimum http://theoreticalminimum.com/
 geometric calculus http://geocalc.clas.asu.edu/pdf-preAdobe8/
 The Geometry and Algebra of Curves on Surfaces Dylan Thurston https://math.berkeley.edu/~qchu/Notes/274/
+Physics topics http://www.astro.uvic.ca/~tatum/index.php
+Theoretical Computer Science's Greatest Hits, 2009 http://www.cs.cmu.edu/~odonnell/hits09/
+MIT Computer Science and Artificial Intelligence Laboratory http://courses.csail.mit.edu/
 
 //literature and art
 箭袋树的舍弃 http://www.ledu365.com/a/shiye/15810.html
@@ -35,10 +39,15 @@ The Geometry and Algebra of Curves on Surfaces Dylan Thurston https://math.berke
 萌纸 http://moekami.himegimi.jp/
 买不起手办送妹子只能自己动手了~ http://bbs.duowan.com/thread-32272934-1-1.html
 数学少女 http://zh.wikipedia.org/wiki/%E6%95%B8%E5%AD%B8%E5%B0%91%E5%A5%B3
+风滚草 http://baike.baidu.com/subview/606905/19750281.htm?fromenter=Tumbleweed&subLemmaId=606905
+quotes http://pages.cs.wisc.edu/~remzi/quotes.html
+The Lake through Trees in Late Autumn http://www.math.northwestern.edu/~len/photos/pages/Evanston/2021213-3.html
 
 //memorable
 Keyto9 http://tieba.baidu.com/p/2098512232
 那稀有的铱 http://www.baidu.com/p/那稀有的铱?from=tieba
+橙的记事本 http://sell.moe/
+Récoltes et Semailles http://acm.math.spbu.ru/RS/
 
 //genernal algebra
 Light's associativity test https://en.wikipedia.org/wiki/Light%27s_associativity_test
@@ -57,6 +66,23 @@ How to put 9 pigs into 4 pens so that there are an odd number of pigs in each pe
 
 //math history
 WHY EISENSTEIN PROVED THE EISENSTEIN CRITERION AND WHY SCHONEMANN DISCOVERED IT FIRST http://dacox.people.amherst.edu/normat.pdf
+周髀算經 - Zhou Bi Suan Jing http://ctext.org/zhou-bi-suan-jing
 
 //category theory
 technical proposal http://math.mit.edu/~dspivak/technical_proposalONR--2013.pdf
+
+//combinatorics
+【科普呀科普】神马是多项式方法 http://tieba.baidu.com/p/2382078174?pid=33788771763&cid=0#33788771763
+
+//ui design
+aboutcg http://www.aboutcg.com/
+
+//math roadmap
+Path to Basics in Algebraic Geometry from HS Algebra and Calculus? http://math.stackexchange.com/questions/285201/path-to-basics-in-algebraic-geometry-from-hs-algebra-and-calculus/285355#285355
+
+//osu
+饮水思源音游版 http://bbs.sjtu.edu.cn/bbstdoc?board=MusicGame
+
+//general topology
+Why is a topology made up of 'open' sets? [closed] http://mathoverflow.net/questions/19152/why-is-a-topology-made-up-of-open-sets
+How to understand the concept of compact space [closed] http://mathoverflow.net/questions/25977/how-to-understand-the-concept-of-compact-space
