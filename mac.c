eca60d9908078c5fa08697c1ba861161ef34d946
coconut32 <coconut_liu@126.com>
2016-05-28 17:44:22 +0800

    Update mac.c

--- mac.c
+++ mac.c
@@ -17,6 +17,7 @@ dbfin http://dbfin.com/
 
 //math blog
 fight with infinity zx31415.wordpress.com
+The Pasqualian http://thepasqualian.com/
 
 //ebook
 Project Gutenberg http://www.gutenberg.org/
@@ -24,12 +25,20 @@ Project Gutenberg http://www.gutenberg.org/
 intersection theory http://math.stanford.edu/~vakil/11-245/
 Expository Notes - J.S. Milne http://www.jmilne.org/math/xnotes/
 Algebraic Combinatorics http://www.math.cornell.edu/~levine/18.312/
+The Theoretical Minimum http://theoreticalminimum.com/
+geometric calculus http://geocalc.clas.asu.edu/pdf-preAdobe8/
+The Geometry and Algebra of Curves on Surfaces Dylan Thurston https://math.berkeley.edu/~qchu/Notes/274/
 
-//literature
+//literature and art
 箭袋树的舍弃 http://www.ledu365.com/a/shiye/15810.html
+求一首英文歌 用了修辞手法的 http://zhidao.baidu.com/question/511787986.html?fr=qrl&index=3
+萌纸 http://moekami.himegimi.jp/
+买不起手办送妹子只能自己动手了~ http://bbs.duowan.com/thread-32272934-1-1.html
+数学少女 http://zh.wikipedia.org/wiki/%E6%95%B8%E5%AD%B8%E5%B0%91%E5%A5%B3
 
 //memorable
 Keyto9 http://tieba.baidu.com/p/2098512232
+那稀有的铱 http://www.baidu.com/p/那稀有的铱?from=tieba
 
 //genernal algebra
 Light's associativity test https://en.wikipedia.org/wiki/Light%27s_associativity_test
@@ -39,3 +48,15 @@ Caccioppoli set
 
 //math game
 15 puzzle https://en.wikipedia.org/wiki/15_puzzle
+随机迷宫 http://blog.sina.com.cn/s/blog_8d1adcb30101e5ox.html
+代数拓扑学在绳子谜题、魔术和巧环中的应用 http://www.docin.com/p-618080097.html
+How to put 9 pigs into 4 pens so that there are an odd number of pigs in each pen? http://math.stackexchange.com/questions/555024/how-to-put-9-pigs-into-4-pens-so-that-there-are-an-odd-number-of-pigs-in-each-pe
+
+//physics
+淺談 Stokes' 定理與電磁學 http://episte.math.ntu.edu.tw/articles/mm/mm_18_4_02/
+
+//math history
+WHY EISENSTEIN PROVED THE EISENSTEIN CRITERION AND WHY SCHONEMANN DISCOVERED IT FIRST http://dacox.people.amherst.edu/normat.pdf
+
+//category theory
+technical proposal http://math.mit.edu/~dspivak/technical_proposalONR--2013.pdf
