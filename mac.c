7928c37b5b5704152311d0455008d4aebc8cb171
coconut32 <coconut_liu@126.com>
2016-05-28 21:36:04 +0800

    Update mac.c

--- mac.c
+++ mac.c
@@ -1,5 +1,51 @@
 //android
 Using PhoneStateListener to listen to state change in Android http://www.devlper.com/2010/06/using-phonestatelistener-to-listen-to-state-change-in-android/
+经典Android开发源代码大全汇总 http://tieba.baidu.com/p/3084892156
+IPC框架分析 Binder，Service，Service manager http://www.linuxidc.com/Linux/2011-04/33966p6.htm
+Fix Android Fragment Error http://www.newthinktank.com/2014/04/fix-android-fragment-error/
+android基础——对Java中注释/**@hide*/的初步认识 http://blog.csdn.net/coco_0707/article/details/17542147
+论停止Java线程的方法 http://blog.csdn.net/mikeszhang/article/details/8751355
+Java Thread.interrupt 害人！ 中断JAVA线程（zz） http://www.blogjava.net/jinfeng_wang/archive/2008/04/27/196477.html
+JAVA中断线程三大基本方法 http://jingyan.baidu.com/article/d621e8dae5093a2865913fab.html
+Android的蓝牙串口(Bluetooth SPP)使用 http://billhsu.me/?p=118
+How to prevent Android bluetooth RFCOMM connection from dying immediately after .connect()? http://stackoverflow.com/questions/2660968/how-to-prevent-android-bluetooth-rfcomm-connection-from-dying-immediately-after/4542466
+让你的Android彻底裸奔吧!! -- 瘫痪Android的安全机制 http://wendal.net/321.html
+安卓开发经验分享：资源、UI、函数库、测试、构建一个都不能少 http://www.importnew.com/4868.html
+Android开发经验汇总 http://www.cnblogs.com/anyanran/archive/2010/11/08/androidex.html
+高手速成android开源项目【项目篇】 http://blog.csdn.net/sam_zhang1984/article/details/25507777
+圆角 + 边框 + 渐变 Button http://weich-javadeveloper.iteye.com/blog/1123042
+Android UI设计 按钮美化 http://blog.sina.com.cn/s/blog_89429f6d01012h3d.html
+Android开发系列---UI篇---简单的圆角边框登录界面 http://www.cnblogs.com/huren/archive/2012/03/27/2419102.html
+封装的力量：封装圆角矩形Button http://www.apkbus.com/blog-76214-41952.html
+Android:Deprecated Thread methods are not supported http://www.th7.cn/Program/java/2012/03/21/65384.shtml
+Android系统默认Home应用程序（Launcher）的启动过程源代码分析 http://blog.csdn.net/luoshengyang/article/details/6767736
+Android——Launcher（桌面启动器）相关知识总结贴 http://www.apkbus.com/android-143706-1-1.html
+Launcher源码浅析-----Launcher布局 http://blog.csdn.net/stevenhu_223/article/details/9842115
+Android中源码Launcher主屏幕程序排列详解【安卓Launcher进化一】 http://zhangkun716717-126-com.iteye.com/blog/1842917
+Launcher与普通Activity的区别 http://blog.csdn.net/bamlook/article/details/11144819
+android实现原理说明之一_application Intent Task Activity http://blog.csdn.net/chenzheng_java/article/details/6215986
+setContentView( )方法 http://blog.sina.com.cn/s/blog_8191005601019pwo.html
+Android框架浅析之锁屏(Keyguard)机制原理 http://blog.csdn.net/qinjuning/article/details/7505703
+Android 实用工具Hierarchy Viewer实战 http://tech.it168.com/a2011/0802/1226/000001226690.shtml
+Android学习笔记（1）—— Android架构剖析 http://blog.csdn.net/osnetdev/article/details/8828271
+Launcher源码浅析-----Launcher启动时的资源加载 http://blog.csdn.net/stevenhu_223/article/details/19416303
+Android开发实践：多级列表的封装与应用 http://www.linuxidc.com/Linux/2013-09/90535.htm
+Android开发---制作桌面可移动控件 http://www.linuxidc.com/Linux/2012-08/68657.htm
+Android Activity中重写onCreateOptionsMenu不显示菜单按钮 http://www.cnblogs.com/xinye/archive/2013/03/13/2958126.html
+Android中的MessageQueue,Handler,Looper和Thread http://wenku.baidu.com/view/049081ce2cc58bd63086bd00.html
+Android中的yaffs2文件系统与文件操作底层实现 http://blog.csdn.net/bamlook/article/details/12047221
+Android中调用Sqlite数据库 http://blog.csdn.net/bamlook/article/details/11226569
+Launcher源码浅析-----涉及Workspace界面资源加载重要类和资源文件简介 http://blog.csdn.net/stevenhu_223/article/details/19971609
+Android开发：ImageView阴影和图层效果 http://www.linuxidc.com/Linux/2011-08/41778.htm
+Android之SurfaceView学习（一） http://www.cnblogs.com/xuling/archive/2011/06/06/android.html
+Android开发之SurfaceView http://blog.csdn.net/redoffice/article/details/6722067
+精美Android UI界面源码（有图有真相） http://www.apkbus.com/android-60664-1-1.html
+移动端界面设计之尺寸篇 http://www.xueui.cn/tutorials/app-tutorials/mobile-ui-design-size.html
+学UI入门阶段到底要掌握哪些知识（UI第一阶段课程） http://www.xueui.cn/design-theory/learn-ui-1.html
+Android切图（点9） http://www.xueui.cn/tutorials/app-tutorials/android-work-tips-qietu.html
+Can an Android Toast be longer than “Toast.LENGTH_LONG”? http://stackoverflow.com/questions/2220560/can-an-android-toast-be-longer-than-toast-length-long
+Android高手进阶教程(十六)之---Android中万能的BaseAdapter(Spinner,ListView,GridView)的使用！ http://blog.csdn.net/Android_Tutor/article/details/5707835
+开启真机的View Server引入HierarchyViewer/By写monkeyrunner自动化测试脚本 https://dup2.org/node/1538
 
 //reverse
 就算Lua也hook给你看 http://bbs.pediy.com/showthread.php?t=163435
@@ -32,6 +78,7 @@ The Geometry and Algebra of Curves on Surfaces Dylan Thurston https://math.berke
 Physics topics http://www.astro.uvic.ca/~tatum/index.php
 Theoretical Computer Science's Greatest Hits, 2009 http://www.cs.cmu.edu/~odonnell/hits09/
 MIT Computer Science and Artificial Intelligence Laboratory http://courses.csail.mit.edu/
+深入浅出通信原理 http://bbs.c114.net/thread-394879-1-1.html#pid4546802
 
 //literature and art
 箭袋树的舍弃 http://www.ledu365.com/a/shiye/15810.html
@@ -42,12 +89,16 @@ MIT Computer Science and Artificial Intelligence Laboratory http://courses.csail
 风滚草 http://baike.baidu.com/subview/606905/19750281.htm?fromenter=Tumbleweed&subLemmaId=606905
 quotes http://pages.cs.wisc.edu/~remzi/quotes.html
 The Lake through Trees in Late Autumn http://www.math.northwestern.edu/~len/photos/pages/Evanston/2021213-3.html
+ALCOHOL: THE SHORTEST WAY OUT OF MANCHESTER http://www.newyorker.com/magazine/1960/01/16/alcohol-the-shortest-way-out-of-manchester
+aboutcg http://www.aboutcg.com/
+Crystallize http://www.xiami.com/song/1770892855
 
 //memorable
 Keyto9 http://tieba.baidu.com/p/2098512232
 那稀有的铱 http://www.baidu.com/p/那稀有的铱?from=tieba
 橙的记事本 http://sell.moe/
 Récoltes et Semailles http://acm.math.spbu.ru/RS/
+linsk http://www.linsk.us/index.php
 
 //genernal algebra
 Light's associativity test https://en.wikipedia.org/wiki/Light%27s_associativity_test
@@ -74,9 +125,6 @@ technical proposal http://math.mit.edu/~dspivak/technical_proposalONR--2013.pdf
 //combinatorics
 【科普呀科普】神马是多项式方法 http://tieba.baidu.com/p/2382078174?pid=33788771763&cid=0#33788771763
 
-//ui design
-aboutcg http://www.aboutcg.com/
-
 //math roadmap
 Path to Basics in Algebraic Geometry from HS Algebra and Calculus? http://math.stackexchange.com/questions/285201/path-to-basics-in-algebraic-geometry-from-hs-algebra-and-calculus/285355#285355
 
@@ -86,3 +134,6 @@ Path to Basics in Algebraic Geometry from HS Algebra and Calculus? http://math.s
 //general topology
 Why is a topology made up of 'open' sets? [closed] http://mathoverflow.net/questions/19152/why-is-a-topology-made-up-of-open-sets
 How to understand the concept of compact space [closed] http://mathoverflow.net/questions/25977/how-to-understand-the-concept-of-compact-space
+
+//finite group theory
+Monsieur Mathieu http://www.neverendingbooks.org/monsieur-mathieu
