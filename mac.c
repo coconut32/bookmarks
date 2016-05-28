083bd0b2203313f77e367a3ad00ca360ae5909c8
coconut32 <coconut_liu@126.com>
2016-05-28 17:07:25 +0800

    Update mac.c

--- mac.c
+++ mac.c
@@ -12,5 +12,30 @@ UltraEdit 16.10.0.1028的反破解保护 http://www.pediy.com/kssd/pediy11/11980
 Unofficial Google Advanced Search http://jwebnet.net/advancedgooglesearch.html
 DMCA (Copyright) Complaint to Google https://www.lumendatabase.org/notices/660366#
 
-//msic blog
-program,math and more http://dbfin.com/
+//program blog
+dbfin http://dbfin.com/
+
+//math blog
+fight with infinity zx31415.wordpress.com
+
+//ebook
+Project Gutenberg http://www.gutenberg.org/
+合肥市图书馆 http://www.hflib.gov.cn/e_book/
+intersection theory http://math.stanford.edu/~vakil/11-245/
+Expository Notes - J.S. Milne http://www.jmilne.org/math/xnotes/
+Algebraic Combinatorics http://www.math.cornell.edu/~levine/18.312/
+
+//literature
+箭袋树的舍弃 http://www.ledu365.com/a/shiye/15810.html
+
+//memorable
+Keyto9 http://tieba.baidu.com/p/2098512232
+
+//genernal algebra
+Light's associativity test https://en.wikipedia.org/wiki/Light%27s_associativity_test
+
+//measure theory
+Caccioppoli set
+
+//math game
+15 puzzle https://en.wikipedia.org/wiki/15_puzzle
